d14ada20b065d36ab85652bc8750fa10ceea3737
Moulik Sharma <moulik.sharma@globallogic.com>
2025-11-10 19:12:56 +0530

    Add CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
 import App from "./App";
 import NotificationProvider from "./NotificationProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <NotificationProvider>
     <BrowserRouter>
+      <CssBaseline />
       <App />
     </BrowserRouter>
   </NotificationProvider>
