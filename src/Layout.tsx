d41caf4320e4e0014d51ffe813aa261b404758e7
diyajoshi1179 <diya.joshi@globallogic.com>
2025-11-18 16:50:07 +0530

    header changes

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router-dom";
 const Layout: React.FC = () => {
   return (
     <Box>
-      {/* <NavHeader /> */}
+      <NavHeader />
       {/* <NavSidebar /> */}
       <Box component="main" sx={{ p: 4, ml: 0, minHeight: "100vh", background: "linear-gradient(135deg, #ce93d8, #512da8)" }}>
         <Toolbar />
